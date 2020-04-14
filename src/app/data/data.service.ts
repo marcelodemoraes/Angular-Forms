bc6da0faccaa61146c7d1f0a42758010fd97efbc
Marcelo de Moraes <marcelodemoraes11@gmail.com>
2020-04-14 17:51:13 -0300

    using putsreq to test http requests

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { UserSettings } from './user-settings';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  postUserSettingsForms(userStettings: UserSettings): Observable<UserSettings> {
-    return of(userStettings);
+  postUserSettingsForms(userStettings: UserSettings): Observable<any> {
+    return this.http.post(
+      'https://putsreq.com/HqcbDDOnZSPgQhaXvdEQ',
+      userStettings
+    );
+
+    // return of(userStettings);
   }
 }
